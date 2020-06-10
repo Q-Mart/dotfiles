4ba8568f2f710458b5a8805b2562c3166d8ab4f2
Q-Mart <qumarthjash@gmail.com>
2020-06-10 14:25:11 +0100

    Add windows terminal and powershell cfgs

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 stow -t $HOME git zsh tmux
 
 stow -t $HOME/.config/nvim nvim
+
+cp ./windows_terminal/settings.json /mnt/c/Users/quj/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/
+
+cp ./powershell/Microsoft.PowerShell_profile.ps1 /mnt/c/Users/quj/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Set-Location "E:\Prog"
