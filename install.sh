2afb3e70003ede3431a439d044585291945a105b
Q-Mart <qumarthjash@gmail.com>
2022-11-05 15:31:36 +0000

    Remove git, zsh and nvim as they are now managed by home-manager

--- install.sh
+++ install.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
-stow -t $HOME zsh git
-
-stow -t $HOME/.config/nvim nvim
-
 stow -t $HOME/.config/i3 i3
 # stow -t $HOME/.config/mpd mpd
 # stow -t $HOME/.ncmpcpp/ ncmpcpp
-
-#NVIM stuff
-#mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-#ln -s ~/.vim $XDG_CONFIG_HOME/nvim
 
 echo "complete"
